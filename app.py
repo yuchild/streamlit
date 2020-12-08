2073c167783a9a91f3bd51b6630aedc98cce6fa6
david <yuchild@gmail.com>
2020-12-07 21:34:06 -0800

    updated

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('This is the title!')
+st.header('This is a header!')
+st.subheader('This app is subheader!')
+st.text('Text goes here')
+st.markdown('### This is a markdown')
+st.success('Sucess!')
